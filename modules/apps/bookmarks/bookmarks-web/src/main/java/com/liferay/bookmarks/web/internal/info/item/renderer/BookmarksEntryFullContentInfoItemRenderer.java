ff43fd00856562ca84a988f1923495255056e769
Lu Liu <lu.liu@liferay.com>
2022-04-07 15:49:11 +0800

    LRQA-64470 Annotate the field

--- modules/apps/bookmarks/bookmarks-web/src/main/java/com/liferay/bookmarks/web/internal/info/item/renderer/BookmarksEntryFullContentInfoItemRenderer.java
+++ modules/apps/bookmarks/bookmarks-web/src/main/java/com/liferay/bookmarks/web/internal/info/item/renderer/BookmarksEntryFullContentInfoItemRenderer.java
@@ -62,14 +62,7 @@ public class BookmarksEntryFullContentInfoItemRenderer
 		}
 	}
 
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.bookmarks.web)",
-		unbind = "-"
-	)
-	public void setServletContext(ServletContext servletContext) {
-		_servletContext = servletContext;
-	}
-
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.bookmarks.web)")
 	private ServletContext _servletContext;
 
 }
